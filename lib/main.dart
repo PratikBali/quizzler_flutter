5a892d0bd5b04ff0001b639bd80e557f55b29e8c
Pratik Bali <pratikbali96@gmail.com>
2023-10-06 19:17:42 +0530

    separate question bank

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import './quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,30 +29,36 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-
-  List<bool> answers = [false, true, true];
-
-  List<String> questions = [
-    "You can lead a cow down stairs but not up stairs.",
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-
+  List<Widget> counterKeeper = [];
   int questionNumber = 0;
   int counter = 0;
+  int limiter = quizBrain.questionBank.length - 1;
 
   void quizCheck(answer) {
-    bool correctAnswer = answers[questionNumber];
-    if(counter <= 2) {
+    bool correctAnswer = quizBrain.getaAnswer(questionNumber);
+    if(counter <= limiter) {
       print(counter);
       setState(() {
+        counterKeeper.add(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
+            child: Text(
+              '${counter+1}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 20
+              ),
+            ),
+          )
+        );
         if(correctAnswer == answer) {
           scoreKeeper.add(Icon(Icons.check, color: Colors.green));
         } else {
           scoreKeeper.add(Icon(Icons.close, color: Colors.red));
         }
         counter++;
-        if(counter != 2) {
+        if(counter != limiter) {
           questionNumber++;
         }
       });
@@ -70,7 +77,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -121,6 +128,9 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: counterKeeper,
+        ),
         Row(
           children: scoreKeeper,
         )
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+   String questionText = 'default';
+   bool questionAnswer = false;
+
+   Question(String q, bool a) {
+     questionText = q;
+     questionAnswer = a;
+   }
+}
